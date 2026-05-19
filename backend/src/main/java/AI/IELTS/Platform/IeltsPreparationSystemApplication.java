c6a18a7e8b9f42ebded7a11e434c71014c357d37
Ornab95 <ornabbiswass@gmail.com>
2026-05-19 06:54:58 +0600

    Adding initial Structure

---
+++ backend/src/main/java/AI/IELTS/Platform/IeltsPreparationSystemApplication.java
@@ -0,0 +1,13 @@
+package AI.IELTS.Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeltsPreparationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeltsPreparationSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/AI/IELTS/Platform/IeltsPreparationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package AI.IELTS.Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeltsPreparationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
